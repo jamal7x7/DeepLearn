e053afdf01d1d2af78a6fedf494c569f75faea4d
Jamal <jamal7x7@gmail.com>
2025-03-19 22:01:51 +0000

    shadcn dashboard-01  block added

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,16 @@ import { Manrope } from 'next/font/google';
 import { UserProvider } from '@/lib/auth';
 import { getUser } from '@/lib/db/queries';
 
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ActiveThemeProvider } from "@/components/active-theme";
+import { cookies } from "next/headers";
+import { cn } from "@/lib/utils";
+
+const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
+
 export const metadata: Metadata = {
   title: 'Next.js SaaS Starter',
   description: 'Get started quickly with Next.js, Postgres, and Stripe.',
@@ -15,20 +25,45 @@ export const viewport: Viewport = {
 
 const manrope = Manrope({ subsets: ['latin'] });
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   let userPromise = getUser();
 
+  const cookieStore = await cookies();
+  const activeThemeValue = cookieStore.get("active_theme")?.value;
+  const isScaled = activeThemeValue?.endsWith("-scaled");
+
   return (
     <html
       lang="en"
+      suppressHydrationWarning
       className={`bg-white dark:bg-gray-950 text-black dark:text-white ${manrope.className}`}
     >
-      <body className="min-h-[100dvh] bg-gray-50">
-        <UserProvider userPromise={userPromise}>{children}</UserProvider>
+      <body
+        className={cn(
+          "bg-background overscroll-none font-sans antialiased",
+          activeThemeValue ? `theme-${activeThemeValue}` : "",
+          isScaled ? "theme-scaled" : ""
+        )}
+      >
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          enableColorScheme
+        >
+          <ActiveThemeProvider initialTheme={activeThemeValue}>
+            <UserProvider userPromise={userPromise}>
+
+              {children}
+
+            </UserProvider>
+          </ActiveThemeProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
